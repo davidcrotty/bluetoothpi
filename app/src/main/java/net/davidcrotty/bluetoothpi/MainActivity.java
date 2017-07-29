a9c9d443d63285188b7a3823ac250f9612914a57
David Crotty <david.crotty.work@gmail.com>
2017-07-29 17:29:59 +0100

    Setup data binding for toggle switch

--- app/src/main/java/net/davidcrotty/bluetoothpi/MainActivity.java
+++ app/src/main/java/net/davidcrotty/bluetoothpi/MainActivity.java
@@ -1,13 +1,38 @@
 package net.davidcrotty.bluetoothpi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import net.davidcrotty.bluetoothpi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if(BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setHandler(this);
+    public void checkedChangedListener(View view, boolean checked) {
+    }
     }
 }
