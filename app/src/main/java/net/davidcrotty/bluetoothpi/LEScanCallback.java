b44aa457f7909a203498d5bffb563a5431fad932
David Crotty <david.crotty.work@gmail.com>
2017-07-29 17:30:17 +0100

    Adds basic bluetooth scan on background thread

---
+++ app/src/main/java/net/davidcrotty/bluetoothpi/LEScanCallback.java
@@ -0,0 +1,14 @@
+package net.davidcrotty.bluetoothpi;
+
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+
+/**
+ * Created by David Crotty on 29/07/2017.
+ * <p>
+ * Copyright © 2017 David Crotty - All Rights Reserved
+ */
+
+public class LEScanCallback extends ScanCallback {
+
+}
--- app/src/main/java/net/davidcrotty/bluetoothpi/MainActivity.java
+++ app/src/main/java/net/davidcrotty/bluetoothpi/MainActivity.java
@@ -1,7 +1,11 @@
 package net.davidcrotty.bluetoothpi;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +14,13 @@ import android.view.View;
 import net.davidcrotty.bluetoothpi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String BLUETOOTH_SCAN_THREAD = "BLUETOOTH_SCAN_THREAD";
+    private final int SCAN_DURATION_MS = 1000;
+    private BluetoothAdapter bluetoothAdapter;
+    private LEScanCallback scanCallback;
+    private HandlerThread scanThread;
+    private boolean isScanning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +43,38 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setHandler(this);
-    public void checkedChangedListener(View view, boolean checked) {
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        bluetoothAdapter = manager.getAdapter();
+        scanCallback = new LEScanCallback();
+
+        scanThread = new HandlerThread(BLUETOOTH_SCAN_THREAD);
+        scanThread.start();
     }
+
+    public void checkedChangedListener(View view, boolean checked) {
+        toggleScan(checked);
+    }
+
+    private void toggleScan(boolean shouldScan) {
+        if(shouldScan) {
+            if(isScanning) return;
+            Handler scanHandler = new Handler(scanThread.getLooper());
+            scanHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    bluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+                }
+            }, SCAN_DURATION_MS);
+            scanHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    bluetoothAdapter.getBluetoothLeScanner().startScan(scanCallback);
+                }
+            });
+            isScanning = true;
+        } else {
+            if(isScanning == false) return;
+            bluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+        }
     }
 }
